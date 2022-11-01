1cfc297ae102796293c5e40fb4d717767c827846
yanasekenta <ynsknt0505@gmail.com>
2022-11-02 07:57:28 +0900

    firebase追加

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,7 +6,7 @@ import { getStorage } from "firebase/storage";
 const firebaseApp = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
